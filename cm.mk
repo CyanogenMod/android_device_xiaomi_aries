9e33d32e6a1fc66a7fcb15d6ee089f69c61a93b5
M1cha <sigmaepsilon92@gmail.com>
2013-12-07 19:40:37 +0100

    change brand name from google to Xiaomi
    Change-Id: I68ccab5b006fb3c5de9d775e2e487f78312fa959

--- cm.mk
+++ cm.mk
@@ -14,11 +14,11 @@ $(call inherit-product, device/xiaomi/aries/full_aries.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aries
 PRODUCT_NAME := cm_aries
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 2
 PRODUCT_MANUFACTURER := XIAOMI
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=aries BUILD_FINGERPRINT=google/aries/aries:4.4/KRT16S/920375:user/release-keys PRIVATE_BUILD_DESC="aries-user 4.4 KRT16S 920375 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=aries BUILD_FINGERPRINT=Xiaomi/aries/aries:4.4/KRT16S/920375:user/release-keys PRIVATE_BUILD_DESC="aries-user 4.4 KRT16S 920375 release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch
