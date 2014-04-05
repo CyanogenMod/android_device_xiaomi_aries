9a8b62eb3d7849c1b43977b3b299148f67eb0974
M1cha <sigmaepsilon92@gmail.com>
2014-04-05 15:14:57 +0200

    libaudioamp: add missing dependency
    Change-Id: I232792fe7e64bd6002fbc02a3bb8bd7813608c05

--- libaudioamp/Android.mk
+++ libaudioamp/Android.mk
@@ -12,6 +12,7 @@ LOCAL_MODULE := libaudioamp
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_C_INCLUDES += hardware/qcom/audio-caf/legacy/alsa_sound
 LOCAL_C_INCLUDES += hardware/qcom/audio-caf/legacy/libalsa-intf
