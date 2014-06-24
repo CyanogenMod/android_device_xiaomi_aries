3a098d8c2c6b72fda83e6ceb742241a6dc952d37
Ethan Chen <intervigil@gmail.com>
2014-06-24 13:50:08 -0700

    aries: Remove libacdbloader makefile entry
    Change-Id: I76b1eee7453823c0ac8b809de1dc11bc4aae604a

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -67,7 +67,6 @@ PRODUCT_COPY_FILES := \\
 DEVICE_PACKAGE_OVERLAYS := vendor/$VENDOR/$DEVICE/overlay
 
 PRODUCT_PACKAGES += \\
-    libacdbloader \\
     libqminvapi
 
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
@@ -93,15 +92,6 @@ EOF
 LOCAL_PATH := \$(call my-dir)
 
 ifeq (\$(TARGET_DEVICE),aries)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := libacdbloader
-LOCAL_MODULE_OWNER := qcom
-LOCAL_SRC_FILES := proprietary/lib/libacdbloader.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := libqminvapi
